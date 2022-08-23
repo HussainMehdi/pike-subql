4d3b22e789cb083bd2adac0bfb1f74615605956e
HussainMehdi <csmehdi101@gmail.com>
2022-08-23 11:13:01 +0500

    fixed atob error

---
+++ src/acalaEvmProcessor.ts
@@ -0,0 +1,4 @@
+global.atob = require("atob");
+import { AcalaDatasourcePlugin } from '@subql/acala-evm-processor/dist/index.js'
+exports.AcalaDatasourcePlugin = AcalaDatasourcePlugin;
+export default AcalaDatasourcePlugin; 
